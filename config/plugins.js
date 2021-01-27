1247658d5762a09c5723637bc71bf49ee00f5fc6
willian-lemann <willianleman@hotmail.com>
2021-01-27 00:58:27 -0300

    mudanca no properties

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
-  upload: {
-    provider: 'aws-s3',
-    providerOptions: {
-      accessKeyId: process.env.AWS_KEY_ID,
-      secretAccessKey: process.env.AWS_ACCESS_KEY,
-      region: 'sa-east-1',
-      params: {
-        Bucket: process.env.BUCKET,
-      },
-    },
-  },
+  // upload: {
+  //   provider: 'aws-s3',
+  //   providerOptions: {
+  //     accessKeyId: process.env.AWS_KEY_ID,
+  //     secretAccessKey: process.env.AWS_ACCESS_KEY,
+  //     region: 'sa-east-1',
+  //     params: {
+  //       Bucket: process.env.BUCKET,
+  //     },
+  //   },
+  // },
 });
